d56d5519f7673cf84aba9375e46f86e6b92ba922
Arnav Puranik <puranikarnav@gmail.com>
2021-08-07 18:02:14 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -37,17 +37,17 @@ def stats(update, context):
     disk = psutil.disk_usage('/').percent
     stats = f'<b>╭──「  ⭕️ BOT STATISTICS ⭕️  」</b>\n' \
             f'<b>│</b>\n' \
-            f'<b>├  ⏰ Bot Uptime : {currentTime}</b>\n' \
-            f'<b>├  💾 Total Disk Space : {total}</b>\n' \
-            f'<b>├  📀 Total Used Space : {used}</b>\n' \
-            f'<b>├  💿 Total Free Space : {free}</b>\n' \
-            f'<b>├  🔼 Total Upload : {sent}</b>\n' \
-            f'<b>├  🔽 Total Download : {recv}</b>\n' \
-            f'<b>├  🖥️ CPU : {cpuUsage}%</b>\n' \
-            f'<b>├  🎮 RAM : {memory}%</b>\n' \
-            f'<b>├  💽 DISK : {disk}%</b>\n' \
+            f'<b>├  Bot Uptime : {currentTime}</b>\n' \
+            f'<b>├ Total Disk Space : {total}</b>\n' \
+            f'<b>├ Total Used Space : {used}</b>\n' \
+            f'<b>├ Total Free Space : {free}</b>\n' \
+            f'<b>├ Total Upload : {sent}</b>\n' \
+            f'<b>├ Total Download : {recv}</b>\n' \
+            f'<b>├ CPU : {cpuUsage}%</b>\n' \
+            f'<b>├ RAM : {memory}%</b>\n' \
+            f'<b>├ DISK : {disk}%</b>\n' \
             f'<b>│</b>\n' \
-            f'<b>╰──「 🚸 @AT_BOTs 🚸 」</b>'
+            f'<b>╰──「」</b>'
     update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.HTML)
 
 
